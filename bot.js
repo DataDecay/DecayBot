9f94212c2b81f4d33e8e4035ffefc6272a393e50
DataDecay <149903180+DataDecay@users.noreply.github.com>
2024-09-07 16:01:07 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -138,7 +138,7 @@ bot.on('spawn', (username, message) => {
     bot.on('command', (command, arg, arg2) => {
         switch (command) {
             case "help":
-                bot.chat('/tellraw @a [{"text":"hello, code, creator, ","color":"blue"},{"text":"cloop, stop-cloops,","color":"green"},{"text":" stop","color":"dark_red"}]')
+                bot.chat('/tellraw @a [{"text":"hello, code, creator, ","color":"blue"},{"text":"cloop, stop-cloops, web-chat","color":"green"},{"text":" stop","color":"dark_red"}]')
                 break;
             case "hello":
                 say('Hello World!')
@@ -149,6 +149,18 @@ bot.on('spawn', (username, message) => {
             case "creator":
                 say("DecayBot V1: Creator: DataDecay.")
                 break;
+            case "web-chat":
+                if (validateOwner(arg)) {
+                    say("Go to https://decaybot-2dlw.onrender.com/serverchat")
+                    hash_counter++;
+                    console.log(hash_counter);
+                } else if (validateTrusted(arg)) {
+                    say("Go to https://decaybot-2dlw.onrender.com/serverchat")
+                    hash_counter++;
+                    console.log(hash_counter);
+                } else {
+                    say('Invalid Hash')
+                }
             case "cloop":
             if (validateOwner(arg)) {
                     loops = setInterval(cloop,500, arg2);
