c30eefaf5612b75be96e8a618eb017ad715b58a0
Samuel Wall <149903180+DataDecay@users.noreply.github.com>
2025-03-12 14:19:36 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -159,14 +159,6 @@ bot.on('spawn', (username, message) => {
     	bot.chat("/" + what)
     	}
     bot.on('kickban', (kicker) => {
-        bot = mineflayer.createBot({
-    host: process.argv[4], // minecraft server ip
-    username: process.argv[6], // username to join as if auth is `offline`, else a unique identifier for this account. Switch if you want to change accounts
-    auth: 'offline' // for offline mode servers, you can set this to 'offline'
-    ///port: 35254,              // set if you need a port that isn't 25565
-    // version: false,           // only set if you need a specific version or snapshot (ie: "1.8.9" or "1.16.5"), otherwise it's set automatically
-    // password: '12345678'      // set if you want to use password-based auth (may be unreliable). If specified, the `username` must be an email
-})
         bot.chat("fuck you, " + kicker);
     })
     bot.on('command', (command, arg, arg2) => {
@@ -268,7 +260,16 @@ bot.on('spawn', (username, message) => {
 })
 
 // Log errors and kick reasons:
-bot.on('kicked', console.log)
+bot.on('kicked', () => {
+    bot = mineflayer.createBot({
+    host: process.argv[4], // minecraft server ip
+    username: process.argv[6], // username to join as if auth is `offline`, else a unique identifier for this account. Switch if you want to change accounts
+    auth: 'offline' // for offline mode servers, you can set this to 'offline'
+    ///port: 35254,              // set if you need a port that isn't 25565
+    // version: false,           // only set if you need a specific version or snapshot (ie: "1.8.9" or "1.16.5"), otherwise it's set automatically
+    // password: '12345678'      // set if you want to use password-based auth (may be unreliable). If specified, the `username` must be an email
+})
+})
 bot.on('error', console.log)
 io.on('connection', (socket) => {
     socket.on('send', (msg) => {
