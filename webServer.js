8ad9fad35b184418e91ffad4117c623f68a1f021
Samuel Wall <149903180+DataDecay@users.noreply.github.com>
2025-03-12 23:09:50 -0400

    Update webServer.js

--- webServer.js
+++ webServer.js
@@ -42,10 +42,10 @@ class WebServer {
             this.bot.chat(msg);
         });
         socket.on('trusted', (msg) => {
-        socket.emit('gen', HashUtils.generateTrusted(args[1]));
+        socket.emit('gen', HashUtils.generateTrusted(process.argv[2]));
     });
     socket.on('owner', (msg) => {
-        socket.emit('gen', HashUtils.generateOwner(args[0]));
+        socket.emit('gen', HashUtils.generateOwner(process.argv[3]));
     });
     }
 }
