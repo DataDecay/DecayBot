1cc56dca362ffd59e5ef126640f350ebcf07c2d8
Samuel Wall <149903180+DataDecay@users.noreply.github.com>
2025-03-13 10:27:41 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,7 +23,7 @@ class Bot {
         var core;
         this.bot.on('spawn', () => {
             this.bot.core = new CommandCore(this.bot.entity.position, { x: this.bot.entity.position.x + 16, y: this.bot.entity.position.y + 1, z: this.bot.entity.position.z + 16 }, this.bot);
-            this.bot.say("DecayBot core initialised");
+            say("DecayBot core initialised");
             this.bot.chatAddPattern(
                 /db:(\S+) ?(.+)?/,
                 "command",
@@ -84,7 +84,7 @@ class Bot {
     }
      handleHello() {
         const core = this.bot.core; // Assuming core is set at the bot level
-        this.bot.say("Hello World!");
+        say("Hello World!");
     }
     handleCore(args) {
         const core = this.bot.core; // Assuming core is set at the bot level
