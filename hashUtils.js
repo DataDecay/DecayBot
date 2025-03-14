b6e8a09697c5760eea2410c472d8c0f6b2691c47
Samuel Wall <149903180+DataDecay@users.noreply.github.com>
2025-03-14 14:30:21 -0400

    Update hashUtils.js

--- hashUtils.js
+++ hashUtils.js
@@ -1,19 +1,19 @@
 const { createHash, randomInt } = require('crypto');
 
 class HashUtils {
-    hash_counter = 0;
+    static hash_counter = 0;
     constructor() {
-        hash_counter = randomInt(999);
+        this.constructor.hash_counter = randomInt(999);
     }
     validateOwner(hashin, prefix) {
         let hash = createHash('sha256');
-        hash.update(prefix + hash_counter.toString());
+        hash.update(prefix + this.constructor.hash_counter.toString());
         let digest = hash.digest('hex').substring(0, 5);
         console.log(digest);
         console.log(hashin);
         let allowed = digest === hashin;
         if (allowed){
-            this.hash_counter++;
+            this.constructor.hash_counter++;
         }
         return allowed;
         
@@ -21,26 +21,26 @@ class HashUtils {
 
     validateTrusted(hashin, prefix) {
         let hash = createHash('sha256');
-        hash.update(prefix + hash_counter.toString());
+        hash.update(prefix + this.constructor.hash_counter.toString());
         let digest = hash.digest('hex').substring(0, 5);
         console.log(digest);
         console.log(hashin);
         let allowed = digest === hashin;
         if (allowed){
-            this.hash_counter++;
+            this.constructor.hash_counter++;
         }
         return allowed;
     }
 
     generateOwner(prefix) {
         let hash = createHash('sha256');
-        hash.update(prefix + hash_counter.toString());
+        hash.update(prefix + this.constructor.hash_counter.toString());
         return hash.digest('hex').substring(0, 5);
     }
 
     generateTrusted(prefix) {
         let hash = createHash('sha256');
-        hash.update(prefix + hash_counter.toString());
+        hash.update(prefix + this.constructor.hash_counter.toString());
         return hash.digest('hex').substring(0, 5);
     }
 }
