39e7cb4b8f33a2589c6c37773a5960460a088f6d
Samuel Wall <149903180+DataDecay@users.noreply.github.com>
2025-03-17 14:12:57 -0400

    Update webServer.js

--- webServer.js
+++ webServer.js
@@ -1,38 +1,72 @@
 const http = require('http');
 const fs = require('fs');
+const path = require('path');
 const socketIO = require('socket.io');
 const HashUtilsLib = require('./hashUtils.js');
 
 class WebServer {
-    
+
     constructor(port, bot) {
         this.port = port;
         this.bot = bot;
-        this.HashUtils = new HashUtilsLib;
+        this.HashUtils = new HashUtilsLib();
     }
 
     start() {
         const server = http.createServer(this.handler.bind(this));
         const io = socketIO(server);
 
-        server.listen(this.port);
+        server.listen(this.port, () => {
+            console.log(`Server running on port ${this.port}`);
+        });
 
         io.on('connection', (socket) => {
             this.handleSocketConnection(socket);
         });
     }
 
+    // Serve files, strip query strings and handle MIME types
     handler(req, res) {
+        let pathname = req.url.split('?')[0]; // Remove query string
+        if (pathname === '/') pathname = '/index.html'; // Default page
 
-        fs.readFile(__dirname + req.url, (err, data) => {
+        const filePath = pathname;
+
+        fs.readFile(filePath, (err, data) => {
             if (err) {
-                res.writeHead(404, { 'Content-Type': 'text/html' });
-                return res.end("404 Not Found");
+                // If file not found, fallback to index.html (SPA style)
+                fs.readFile(__dirname + '/public' + pathname, (error, fallbackData) => {
+                    if (error) {
+                        res.writeHead(404, { 'Content-Type': 'text/html' });
+                        return res.end("404 Not Found");
+                    }
+
+                    res.writeHead(200, { 'Content-Type': this.getMimeType('index.html') });
+                    res.end(fallbackData);
+                });
+                return;
             }
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.write(data);
-            return res.end();
+
+            res.writeHead(200, { 'Content-Type': this.getMimeType(filePath) });
+            res.end(data);
         });
+    }
+
+    // Simple MIME type detection
+    getMimeType(filePath) {
+        const ext = path.extname(filePath).slice(1); // Get file extension without the dot
+        const mimeTypes = {
+            'html': 'text/html',
+            'js': 'application/javascript',
+            'css': 'text/css',
+            'json': 'application/json',
+            'png': 'image/png',
+            'jpg': 'image/jpeg',
+            'gif': 'image/gif',
+            'svg': 'image/svg+xml',
+            'ico': 'image/x-icon'
+        };
+        return mimeTypes[ext] || 'application/octet-stream'; // Default to binary stream
     }
 
     handleSocketConnection(socket) {
@@ -44,12 +78,14 @@ class WebServer {
         socket.on('send', (msg) => {
             this.bot.chat(msg);
         });
+
         socket.on('trusted', (msg) => {
-        socket.emit('gen', this.HashUtils.generateTrusted(process.argv[2]));
-    });
-    socket.on('owner', (msg) => {
-        socket.emit('gen', this.HashUtils.generateOwner(process.argv[1]));
-    });
+            socket.emit('gen', this.HashUtils.generateTrusted(process.argv[2]));
+        });
+
+        socket.on('owner', (msg) => {
+            socket.emit('gen', this.HashUtils.generateOwner(process.argv[1]));
+        });
     }
 }
 
