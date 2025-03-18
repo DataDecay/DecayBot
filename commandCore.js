c3c930e3be41301f4ad1b1a4ed0053f5cae6e453
Samuel Wall <149903180+DataDecay@users.noreply.github.com>
2025-03-18 10:56:03 -0400

    Update commandCore.js

--- commandCore.js
+++ commandCore.js
@@ -1,98 +1,44 @@
+// commandcore.js
 class CommandCore {
   constructor(xyz, toxyz, bot) {
-    this.bot = bot;
+    this.xyz = bot.entity.position;//
+    this.toxyz = {x: bot.entity.position.x+16, y: this.xyz.y+1, z: bot.entity.position.z+16};
+    this.useBlockxyz = {x: xyz.x, y: xyz.y, z: xyz.z};
     this.client = bot._client;
-
-    this.xyz = {
-      x: Math.round(bot.entity.position.x),
-      y: Math.round(bot.entity.position.y),
-      z: Math.round(bot.entity.position.z)
-    };
-
-    this.toxyz = {
-      x: this.xyz.x + 16,
-      y: this.xyz.y + 1,
-      z: this.xyz.z + 16
-    };
-
-    this.useBlockxyz = { x: xyz.x, y: xyz.y, z: xyz.z };
     this.i = 1;
-
-    this.refillCore(); // Prepare the blocks when initializing
+    this.refillCore();
   }
 
   refillCore(_xyz = this.xyz, _toxyz = this.toxyz) {
-    this.xyz = {
-      x: Math.round(_xyz.x),
-      y: Math.round(_xyz.y),
-      z: Math.round(_xyz.z)
-    };
-
-    this.toxyz = {
-      x: Math.round(_toxyz.x),
-      y: Math.round(_toxyz.y),
-      z: Math.round(_toxyz.z)
-    };
-
-    const sizeX = Math.abs(this.toxyz.x - this.xyz.x) + 1;
-    const sizeY = Math.abs(this.toxyz.y - this.xyz.y) + 1;
-    const sizeZ = Math.abs(this.toxyz.z - this.xyz.z) + 1;
-
-    let blockCount = 0;
-
-    // Place always-active repeating command blocks using /setblock
-    for (let x = 0; x < sizeX; x++) {
-      for (let y = 0; y < sizeY; y++) {
-        for (let z = 0; z < sizeZ; z++) {
-          const px = this.xyz.x + x;
-          const py = this.xyz.y + y;
-          const pz = this.xyz.z + z;
-
-          const cmd = `/setblock ${px} ${py} ${pz} repeating_command_block[facing=up]{auto:1b}`;
-          this.client.chat(cmd);
-
-          blockCount++;
-        }
-      }
-    }
-
-    console.log(`Refilled ${blockCount} command blocks from (${this.xyz.x}, ${this.xyz.y}, ${this.xyz.z}) to (${this.toxyz.x}, ${this.toxyz.y}, ${this.toxyz.z})`);
+    this.xyz = _xyz = {x: Math.round(_xyz.x), y: Math.round(_xyz.y), z: Math.round(_xyz.z)};
+    this.toxyz = _toxyz = {x: Math.round(_toxyz.x), y: Math.round(_toxyz.y), z: Math.round(_toxyz.z)};
+    this.client.chat(`/fill ${_xyz.x} ${_xyz.y} ${_xyz.z} ${_toxyz.x} ${_toxyz.y} ${_toxyz.z} repeating_command_block`);
   }
 
   run(command) {
-    const trimmedCommand = command.slice(0, 32767); // Just in case it’s huge
+    this.client.write("update_command_block", { location: this.useBlockxyz, command: command.slice(0, 32767), mode: 1, flags: 0x04 });
+    //console.log("choosing cmdblock: ", this.useBlockxyz);
 
-    console.log("Running command:", trimmedCommand);
-    console.log("At location:", this.useBlockxyz);
-
-    this.client.write('update_command_block', {
-  location: this.useBlockxyz, // pick a placed repeating_command_block
-  command: 'say test',
-  mode: 2,   // Auto Mode
-  flags: 0x05 // Track Output + Auto
-});
-
-
-    const coords = indexToCoords(this.i++, this.xyz, this.toxyz);
+    let coords = indexToCoords(this.i++, this.xyz, this.toxyz);
+    //console.log(coords)
     this.useBlockxyz = coords;
   }
 }
 
-// Coordinate math to find where in the grid the next block is
-function indexToCoords(i, start = { x: 0, y: 0, z: 0 }, end = { x: 0, y: 0, z: 0 }) {
-  const sizeX = Math.abs(end.x - start.x) + 1;
-  const sizeY = Math.abs(end.y - start.y) + 1;
-  const sizeZ = Math.abs(end.z - start.z) + 1;
+function indexToCoords(i, start={x:0, y:0, z:0}, end={x: 0, y: 0, z:0}) {
+ let sizeX = Math.abs(end.x - start.x) + 1;
+ let sizeY = Math.abs(end.y - start.y) + 1;
+ let sizeZ = Math.abs(end.z - start.z) + 1;
 
-  const x = i % sizeX;
-  const y = Math.floor(i / (sizeX * sizeZ)) % sizeY;
-  const z = Math.floor(i / sizeX) % sizeZ;
+ let x = i % sizeX;
+ let y = Math.floor(i / (sizeX * sizeZ)) % sizeY;
+ let z = Math.floor(i / sizeX) % sizeZ;
 
-  return {
-    x: start.x + x,
-    y: start.y + y,
-    z: start.z + z
-  };
+ return {
+  x: start.x + x,
+  y: start.y + y,
+  z: start.z + z
+ };
 }
 
 module.exports = CommandCore;
