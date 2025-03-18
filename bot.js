fa6f195edd264263df7d9a5a4f92ca5920cb022d
Samuel Wall <149903180+DataDecay@users.noreply.github.com>
2025-03-18 11:03:51 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,7 +31,7 @@ class Bot {
             const io = new WebServer(config.get("webServer.port"), this.bot, this.HashUtils);
             io.start();
             this.bot.creative.startFlying();
-            this.bot.chat('/op ' + config.get("connection.botName"));
+            await this.bot.chat('/op ' + config.get("connection.botName"));
             this.bot.chat("/tp "+config.get("connection.botName")+" 6000 110 6000");
             
             
