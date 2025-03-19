1d01357665bf77751dd706c6a0d804e9dcce92f3
Samuel Wall <149903180+DataDecay@users.noreply.github.com>
2025-03-19 07:25:35 -0400

    Update commandCore.js

--- commandCore.js
+++ commandCore.js
@@ -12,44 +12,17 @@ class CommandCore {
   refillCore(_xyz = this.xyz, _toxyz = this.toxyz) {
     this.xyz = _xyz = {x: Math.round(_xyz.x), y: Math.round(_xyz.y), z: Math.round(_xyz.z)};
     this.toxyz = _toxyz = {x: Math.round(_toxyz.x), y: Math.round(_toxyz.y), z: Math.round(_toxyz.z)};
-    this.client.chat(`/fill ${_xyz.x} ${_xyz.y} ${_xyz.z} ${_toxyz.x} ${_toxyz.y} ${_toxyz.z} repeating_command_block[facing=up]{auto:1b}`);
+    this.client.chat(`/fill ${_xyz.x} ${_xyz.y} ${_xyz.z} ${_toxyz.x} ${_toxyz.y} ${_toxyz.z} repeating_command_block`);
   }
 
   run(command) {
-  if (!command) return;
+    this.client.write("update_command_block", { location: this.useBlockxyz, command: command.slice(0, 32767), mode: 1, flags: 0x04 });
+    //console.log("choosing cmdblock: ", this.useBlockxyz);
 
-  console.log('Running command:', command);
-
-  // 1. Use current block
-  const coords = this.useBlockxyz;
-
-  // 2. Send update_command_block packet
-  this.client.write('update_command_block', {
-    location: coords,
-    command: command.slice(0, 32767),
-    mode: 1, // auto
-    flags: 0x04 // track output + auto
-  });
-
-  // 3. Increment to next block
-  let nextCoords = indexToCoords(this.i++, this.xyz, this.toxyz);
-
-  // 4. Make sure the next block exists as a command block!
-  // If out of range, refill the area
-  if (
-    nextCoords.x > this.toxyz.x ||
-    nextCoords.y > this.toxyz.y ||
-    nextCoords.z > this.toxyz.z
-  ) {
-    console.log('Refilling command block area!');
-    this.refillCore();
-    this.i = 0;
-    nextCoords = indexToCoords(this.i++, this.xyz, this.toxyz);
+    let coords = indexToCoords(this.i++, this.xyz, this.toxyz);
+    //console.log(coords)
+    this.useBlockxyz = coords;
   }
-
-  this.useBlockxyz = nextCoords;
-}
-
 }
 
 function indexToCoords(i, start={x:0, y:0, z:0}, end={x: 0, y: 0, z:0}) {
