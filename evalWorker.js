e98427fc2124660ea77d7398731a1983072792e3
Samuel Wall <149903180+DataDecay@users.noreply.github.com>
2025-03-20 14:59:52 -0400

    Update evalWorker.js

--- evalWorker.js
+++ evalWorker.js
@@ -1,54 +1,21 @@
 const vm = require('vm');
 
-class evalWorker {
-  constructor(bot) {
-    this.bot = bot;
-    this.sandbox = this.createNewSandbox();
+const sandbox = {
+  input: '2 + 2',
+  result: null,
+};
 
-    const scriptCode = `
-      (function() {
-        try {
-          globalThis.result = eval(globalThis.input);
-        } catch (e) {
-          globalThis.result = "Error: " + e.message;
-        }
-      })();
-    `;
+const context = vm.createContext(sandbox);
 
-    this.script = new vm.Script(scriptCode);
+const code = `
+  try {
+    result = eval(input);
+  } catch (e) {
+    result = "Error: " + e.message;
   }
+`;
 
-  createNewSandbox() {
-    return {
-      result: null,
-      input: null,
-      send: (message) => { this.say(message); },
-      counter: 0,
-      Math: Math,
-    };
-  }
+const script = new vm.Script(code);
+script.runInContext(context);
 
-  say(text, colour = "white") {
-    this.bot.core.run(`tellraw @a [{"text":"${text}","color":"${colour}"}]`);
-  }
-
-  SandboxedEval(input) {
-    return new Promise((resolve, reject) => {
-      this.sandbox.input = input;
-
-      try {
-        this.script.runInNewContext(this.sandbox, { timeout: 5000 });
-        resolve(this.sandbox.result);
-      } catch (error) {
-        reject('Execution failed: ' + error.message);
-      }
-    });
-  }
-
-  ResetWorker() {
-    this.sandbox = this.createNewSandbox();
-    this.say("Worker Reset", "green");
-  }
-}
-
-module.exports = evalWorker;
+console.log('Result:', sandbox.result);  // Should log: 4
