d3047b26e0889ae39797d6230f975be066e3165c
Samuel Wall <149903180+DataDecay@users.noreply.github.com>
2025-03-22 17:17:54 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,9 +9,14 @@ class Bot {
     constructor() {
         this.bot = null;
         this.HashUtils = new HashUtilsLib();
+        this.reconnectDelay = 5000; // 5 seconds delay before reconnecting
     }
 
     start() {
+        this.createBotInstance();
+    }
+
+    createBotInstance() {
         this.bot = mineflayer.createBot({
             host: config.get("connection.serverName"),
             username: config.get("connection.botName"),
@@ -22,20 +27,19 @@ class Bot {
         this.client = this.bot._client;
 
         this.bot.on('spawn', () => {
+            console.log("Bot has spawned");
             this.bot.chatAddPattern(/db:(\S+) ?(.+)?/, "command", "Command Sent");
 
             const io = new WebServer(config.get("webServer.port"), this.bot, this.HashUtils);
             io.start();
 
             this.bot.creative.startFlying();
-            this.client.chat("/tp DecayBot 6000 -49 6000");
+            this.client.chat("/tp " + config.get("connection.botName") + " 6000 -49 6000");
 
-            // Initialize Command Core and Parser
             this.bot.core = new CommandCore({ x: 6000, y: -50, z: 6000 }, { x: 6010, y: -52, z: 6010 }, this.bot);
             this.bot.core.refillCore({ x: 6000, y: -50, z: 6000 }, { x: 6010, y: -52, z: 6010 }, this.bot);
             this.commandParser = new CommandParser(this.bot, this.HashUtils);
 
-            // Listen for commands
             this.bot.on('command', async (command, argsraw) => {
                 console.log(command + ", " + argsraw);
                 if (command === "help") {
@@ -44,16 +48,34 @@ class Bot {
                     await this.commandParser.handleCommand(command, argsraw ? argsraw.split(" ") : []);
                 }
             });
+
             this.bot.on('death', () => {
                 this.client.chat("/tp " + config.get("connection.botName") + " 6000 -49 6000");
+            });
         });
 
-            this.bot.on('error', console.log);
+        this.bot.on('error', (err) => {
+            console.log("Bot encountered an error:", err);
+            this.reconnect();
+        });
+
+        this.bot.on('end', () => {
+            console.log("Bot disconnected, attempting to reconnect...");
+            this.reconnect();
         });
     }
 
+    reconnect() {
+        console.log(`Reconnecting in ${this.reconnectDelay / 1000} seconds...`);
+        setTimeout(() => {
+            this.createBotInstance();
+        }, this.reconnectDelay);
+    }
+
     say(text, colour = "white") {
-        this.bot.core.run(`tellraw @a [{"text":"${text}","color":"${colour}"}]`);
+        if (this.bot && this.bot.core) {
+            this.bot.core.run(`tellraw @a [{"text":"${text}","color":"${colour}"}]`);
+        }
     }
 }
 
