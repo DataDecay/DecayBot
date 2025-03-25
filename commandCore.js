0b0c68e76fb4ac36fdb106d953110d7662d800ac
Samuel Wall <149903180+DataDecay@users.noreply.github.com>
2025-03-25 07:29:09 -0400

    Update commandCore.js

--- commandCore.js
+++ commandCore.js
@@ -1,4 +1,3 @@
-// commandcore.js
 class CommandCore {
   constructor(xyz, toxyz, bot) {
     this.xyz = bot.entity.position;//
@@ -17,7 +16,7 @@ class CommandCore {
 
   run(command) {
     this.client.write("update_command_block", { location: this.useBlockxyz, command: command.slice(0, 32767), mode: 1, flags: 0x04 });
-    //console.log("choosing cmdblock: ", this.useBlockxyz);
+    //console.log("choosing cmdblock:", this.useBlockxyz);
 
     let coords = indexToCoords(this.i++, this.xyz, this.toxyz);
     //console.log(coords)
