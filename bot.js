98431cbf1d7ccdba2dd6130f712b3413f4d484c9
Samuel Wall <149903180+DataDecay@users.noreply.github.com>
2025-04-05 17:26:48 -0400

    set up autorestart i guess
    I love vs code :D

--- bot.js
+++ bot.js
@@ -4,16 +4,20 @@ const HashUtilsLib = require('./hashUtils.js');
 const WebServer = require('./webServer.js');
 const CommandParser = require('./commandParser.js');
 const config = require('config');
+const fs = require('fs');
+const path = require('path');
 
 class Bot {
     constructor() {
         this.bot = null;
         this.HashUtils = new HashUtilsLib();
-        this.reconnectDelay = 5000; // yeh this delay sucks
+        this.reconnectDelay = 5000;
+        this.flagPath = path.join(__dirname, 'flag.json');
     }
 
     start() {
         this.createBotInstance();
+        this.setupAutoRestart();
     }
 
     createBotInstance() {
@@ -33,23 +37,22 @@ class Bot {
             io.start();
             this.bot.creative.startFlying();
             try {
-            this.client.chat("/tp " + config.get("connection.botName") + " 6000 -49 6000");
+                this.client.chat("/tp " + config.get("connection.botName") + " 6000 -49 6000");
             } catch {
-            this.client = this.bot._client;
-            this.client.chat("/tp " + config.get("connection.botName") + " 6000 -49 6000");
+                this.client = this.bot._client;
+                this.client.chat("/tp " + config.get("connection.botName") + " 6000 -49 6000");
             }
 
             this.bot.core = new CommandCore({ x: 6000, y: -50, z: 6000 }, { x: 6010, y: -52, z: 6010 }, this.bot);
-            
             this.commandParser = new CommandParser(this.bot, this.HashUtils);
-            setTimeout(()=>{
-            this.bot.core.refillCore({ x: 6000, y: -50, z: 6000 }, { x: 6010, y: -52, z: 6010 }, this.bot);
+            setTimeout(() => {
+                this.bot.core.refillCore({ x: 6000, y: -50, z: 6000 }, { x: 6010, y: -52, z: 6010 }, this.bot);
             }, 1000);
+
             this.bot.on('command', async (command, argsraw) => {
-                
                 console.log(command + ", " + argsraw);
                 if (command === "help") {
-                    this.commandParser.showHelp(argsraw);  //todo combine
+                    this.commandParser.showHelp(argsraw);
                 } else {
                     await this.commandParser.handleCommand(command, argsraw ? argsraw.split(" ") : []);
                 }
@@ -62,19 +65,59 @@ class Bot {
 
         this.bot.on('error', (errrrr) => {
             console.log(errrrr);
-            return;
+            this.updateFlag('restart', true);
+            this.reconnect();
         });
 
         this.bot.on('end', () => {
-            return;
+            console.log('Bot disconnected');
+            this.updateFlag('restart', true);
+            this.reconnect();
         });
     }
 
     reconnect() {
-        console.log(`Reconnecting`);
+        console.log(`Reconnecting...`);
         setTimeout(() => {
             this.start();
         }, this.reconnectDelay);
+    }
+
+    updateFlag(key, value) {
+        fs.readFile(this.flagPath, 'utf8', (err, data) => {
+            if (err) {
+                console.error('Error reading flag.json:', err);
+                return;
+            }
+
+            const flags = JSON.parse(data);
+            flags[key] = value;
+            flags.last = new Date().toISOString();
+            fs.writeFile(this.flagPath, JSON.stringify(flags, null, 2), (err) => {
+                if (err) console.error('Error writing flag.json:', err);
+            });
+        });
+    }
+
+    setupAutoRestart() {
+        const fiveHoursInMs = 5 * 60 * 60 * 1000;
+        setInterval(() => {
+            const currentTime = new Date();
+            fs.readFile(this.flagPath, 'utf8', (err, data) => {
+                if (err) {
+                    console.error('Error reading flag.json:', err);
+                    return;
+                }
+
+                const flags = JSON.parse(data);
+                if (flags.last) {
+                    const last = new Date(flags.last);
+                    if (currentTime - last >= fiveHoursInMs) {
+                        this.updateFlag('restart', true);
+                    }
+                }
+            });
+        }, 60 * 1000);
     }
 
     say(text, colour = "white") {
