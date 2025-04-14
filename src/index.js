565e85274cb86d59571d553433542376fbfcf846
Samuel Wall <149903180+DataDecay@users.noreply.github.com>
2025-04-14 14:12:48 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const bot = new Bot();
 var argv = require('minimist')(process.argv.slice(2));
 if(argv.verbose){
     global.v = true;
-    console.warn("Verbose enabled - potentially sensitive information in STDOUT!")
+    console.warn("Verbose enabled -- potentially sensitive information in STDOUT!")
 } else {
     global.v = false;
 }
